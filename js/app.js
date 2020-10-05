8060589f8ca371f5c649d410e72cd1a0a2a15096
Daisy Watson <Daisy@Daisys-Air.home>
2020-10-05 12:29:43 -0400

    got values from radio buttons

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
-console.log("linked");
+//console.log("linked");
+const startGame = ($modal) => {
+  //Hide the instructions
+  $modal.hide();
+
+  let gameMode = $("input[name='game-mode']:checked").val();
+  console.log(gameMode)
+  let displayDirection = $("input[name='display-direction']:checked").val();
+  console.log(displayDirection)
+  let listType = $("input[name='list-type']:checked").val();
+  console.log(listType)
+  let scenario = $("input[name='scenario']:checked").val();
+  console.log(scenario)
+}
+
 
 $(() => {
-
+  const $modal = $('#modal');
+  $('#startBtn').on('click', () => {
+    startGame($modal)
+  });
 });
